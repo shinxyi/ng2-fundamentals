d68d2f0f171122c0d9a954ed955b495be5155d29
ShinYi Tan <shinxyi.tan@gmail.com>
2017-03-29 16:28:22 -0500

    Fiddling with New Template Syntax

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,16 +6,26 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         <div class="well hoverwell thumbnail">
             <h2>{{event.name}}</h2>
             <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <div [ngSwitch]="event.time">
+                Time: {{event.time}}
+                <span *ngSwitchCase="'8:00 am'"> (Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'"> (Late Start)</span>
+                <span *ngSwitchDefault> (Normal Start)</span>
             </div>
+            <div>Price: \${{event.price}}</div>
+            <div *ngIf="event.location">
+                <span>Location: {{event.location?.address}}</span>
+                <span class="pad-left">{{event.location?.city}}, {{event.location?.country}}</span>
+            </div>
+            <div *ngIf="event.onlineUrl">
+                <span>URL: {{event.onlineUrl}}</span>
+            </div>
+
             <!--<button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>-->
         </div>
     `,
     styles: [`
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
     `]
